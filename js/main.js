af8e923572b83bb63dba6cd891bcec75fb96770b
dylanmaupa <114060288+dylanmaupa@users.noreply.github.com>
2024-06-20 15:10:09 +0200

    changes

--- js/main.js
+++ js/main.js
@@ -1,12 +1,41 @@
-// var swiper = new Swiper(".mySwiper", {
-//     // slidesPerView: 3,
-//     spaceBetween: 30,
-//     pagination: {
-//       el: ".swiper-pagination",
-//       clickable: true,
-//     },
-//     navigation: {
-//         nextEl: ".swiper-button-next",
-//         prevEl: ".swiper-button-prev",
-//       },
-//   });
+let circleProg = document.querySelector(".progress");
+let progValue = document.querySelector(".progValue");
+
+let progStartValue = 0,
+  progEndValue = 94,
+  speed = 50;
+
+let progress = setInterval(() => {
+  progStartValue++;
+
+  progValue.textContent = `${progStartValue}%`;
+  circleProg.style.background = `conic-gradient(#2193D1 ${
+    progStartValue * 3.6
+  }deg, #ccc 0deg)`;
+
+  if (progStartValue === progEndValue) {
+    clearInterval(progress);
+  }
+  console.log(progStartValue);
+}, speed);
+
+let circleProg2 = document.querySelector(".progress2");
+let progValue2 = document.querySelector(".progValue2");
+
+let progStartValue2 = 0,
+  progEndValue2 = 92,
+  speed2 = 50;
+
+let progress2 = setInterval(() => {
+  progStartValue2++;
+
+  progValue2.textContent = `${progStartValue2}%`;
+  circleProg2.style.background = `conic-gradient(#2193D1 ${
+    progStartValue2 * 3.6
+  }deg, #ccc 0deg)`;
+
+  if (progStartValue2 === progEndValue2) {
+    clearInterval(progress2);
+  }
+  console.log(progStartValue2);
+}, speed2);
